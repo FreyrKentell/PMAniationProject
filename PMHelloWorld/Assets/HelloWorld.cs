dcfa659ce097b2c2278fec6704dd8f5e0f951fb8
Isaac Ortiz <19.isaac.ortiz@ksd.org>
2019-10-17 13:45:46 -0700

    AnimationPush

---
+++ PMHelloWorld/Assets/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I only have 5 HP and I need to find ten new friends!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PMHelloWorld/Assets/IfStatements.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    public float coffeeTemp = 170.0f;
+    float hotTemp = 180.0f;
+    float coldTemp = 110.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        coffeeTemp -= Time.deltaTime * 5;
+        //coffee = coffeTemp - Time.deltaTime * 5
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            TempatureTest();
+        }
+    }
+    void TempatureTest()
+    {
+        if(coffeeTemp > hotTemp)
+        {
+            Debug.Log("Ouch! I Burned My Tongue...");
+        }
+        else if(coffeeTemp < coldTemp)
+        {
+            Debug.Log("Ugh, COLD coffee is groooossss.");
+        }
+        else
+        {
+            //just right
+            Debug.Log("Mmmmmmm Coffeee Coffeee Coffeeeeee");
+        }
+    }
+}
